80007c48be0e72da183ec2a76ddf1e447bef397c
Fabio Barteri <frbarteri@gmail.com>
2022-04-24 17:33:02 +0200

    Updated homepage and info
    Updated homepage and general info

---
+++ _posts/2022-04-24-new_blog_from_scratch.md
@@ -0,0 +1,17 @@
+---
+title: "Building a new blog from scratch."
+categories:
+  - Daylife
+tags:
+  - blog
+---
+
+Hi everyone and welcome to linudz:home. I am building this website and testing cool things such as *italic* and **bold**
+
+```python:
+
+print("or maybe just some code here, since")
+
+if you > "noob":
+    youmust.write("code")
+```
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Building a new blog from scratch.",
+        "excerpt":"Hi everyone and welcome to linudz:home. I am building this website and testing cool things such as italic and bold    print(\"or maybe just some code here, since\")  if you &gt; \"noob\":     youmust.write(\"code\")  ","categories": ["Daylife"],
+        "tags": ["blog"],
+        "url": "/daylife/new_blog_from_scratch/",
+        "teaser": null
+      }]
